0655195b7fc99615b189d8fafda64f214b741f56
Vaibhav-Kumar-K-R <vaibhavkumar.k.r.cnr@gmail.com>
2025-10-13 21:10:47 +0530

    Added common folder of shared components for reusable

---
+++ frontend/src/components/common/Footer.tsx
@@ -0,0 +1,17 @@
+function Footer() {
+    return (
+        <footer className="static bottom-1 left-0 flex w-full justify-center sm:fixed">
+            <span>
+                Developed by{" "}
+                <a
+                    href="https://github.com/Vaibhav-Kumar-K-R"
+                    className="text-primary underline underline-offset-1"
+                >
+                  Vaibhav Kumar K R
+                </a>
+            </span>
+        </footer>
+    )
+}
+
+export default Footer
